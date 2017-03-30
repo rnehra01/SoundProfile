29316c7e343b28fde0b526158af46fe20372be14
Ravinder Nehra <hnehra1@gmail.com>
2017-03-30 14:45:38 +0530

    Add button

--- app/src/main/java/com/example/csn254/soundprofile/Home.java
+++ app/src/main/java/com/example/csn254/soundprofile/Home.java
@@ -2,12 +2,26 @@ package com.example.csn254.soundprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    ImageButton floatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        floatButton = (ImageButton) findViewById(R.id.addButton);
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "This feature will be available soon", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
