d931a2f2696a05b2c64c416825428a737d0b0ae9
nisheeth-golakiya <nisheeth.golakiya@gmail.com>
2017-04-02 16:36:25 +0530

    Add time input UI

--- app/src/main/java/com/example/csn254/soundprofile/add_slot.java
+++ app/src/main/java/com/example/csn254/soundprofile/add_slot.java
@@ -2,7 +2,9 @@ package com.example.csn254.soundprofile;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.Dialog;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -21,64 +24,144 @@ import java.util.Calendar;
  */
 
 public class add_slot extends AppCompatActivity {
+    private TextView start_time_display;
+    private TextView end_time_display;
+    private Button pick_start_time;
+    private Button pick_end_time;
+    private int pHour;
+    private int pMinute;
+    private int TIME_DIALOG_ID = 0;
     AlarmManager alarm_manager;
     TimePicker alarm_timepicker;
     Context context;
     PendingIntent pending_intent;
+
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    pHour = hourOfDay;
+                    pMinute = minute;
+                    updateDisplay();
+                    //displayToast();
+                }
+            };
+
+    /** Updates the time in the TextView */
+    private void updateDisplay() {
+        if (TIME_DIALOG_ID == 0) {
+            start_time_display.setText(
+                    new StringBuilder()
+                            .append(pad(pHour)).append(":")
+                            .append(pad(pMinute)));
+        }else{
+            end_time_display.setText(
+                    new StringBuilder()
+                            .append(pad(pHour)).append(":")
+                            .append(pad(pMinute)));
+        }
+
+    }
+
+    /** Displays a notification when the time is updated */
+//    private void displayToast() {
+//        Toast.makeText(this, new StringBuilder().append("Time choosen is ").append(displayTime.getText()),   Toast.LENGTH_SHORT).show();
+//
+//    }
+
+    /** Add padding to numbers less than ten */
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_slot);
         this.context = this;
 
+        start_time_display = (TextView) findViewById(R.id.start_time_display);
+        end_time_display = (TextView) findViewById(R.id.end_time_display);
+        pick_start_time = (Button) findViewById(R.id.start_time);
+        pick_end_time = (Button) findViewById(R.id.end_time);
+
+        pick_start_time.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TIME_DIALOG_ID = 0;
+                showDialog(0);
+            }
+        });
+
+        pick_end_time.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TIME_DIALOG_ID = 1;
+                showDialog(0);
+            }
+        });
+
+
         //initialize our alarm manager
         alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         //initialize our time picker
-        alarm_timepicker = (TimePicker) findViewById(R.id.clock);
+        //alarm_timepicker = (TimePicker) findViewById(R.id.clock);
         final Calendar calendar = Calendar.getInstance();
 
         final Intent my_intent = new Intent(this.context, Alarm_Receiver.class);
 
         Button start_alarm = (Button) findViewById(R.id.save_button);
 
-        start_alarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //setting calendar instance with the hour and minute that we picked
-                //on tne time picker
-                calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getCurrentHour());
-                calendar.set(Calendar.MINUTE, alarm_timepicker.getCurrentMinute());
-
-                //get the string value of the hour and minute
-                int hour = alarm_timepicker.getCurrentHour();
-                int minute = alarm_timepicker.getCurrentMinute();
-
-                //convert the int values to strings
-                String hour_string = String.valueOf(hour);
-                String minute_string = String.valueOf(minute);
-
-                //convert 24-hour to 12-hour time
-                /*if(hour>12)
-                    hour_string = String.valueOf(hour-12);
-                if(minute<10){
-                    minute_string = "0" + String.valueOf(minute);
-                }*/
-
-                pending_intent = PendingIntent.getBroadcast(add_slot.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
-
-            }
-        });
+//        start_alarm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //setting calendar instance with the hour and minute that we picked
+//                //on tne time picker
+//                calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getCurrentHour());
+//                calendar.set(Calendar.MINUTE, alarm_timepicker.getCurrentMinute());
+//
+//                //get the string value of the hour and minute
+//                int hour = alarm_timepicker.getCurrentHour();
+//                int minute = alarm_timepicker.getCurrentMinute();
+//
+//                //convert the int values to strings
+//                String hour_string = String.valueOf(hour);
+//                String minute_string = String.valueOf(minute);
+//
+//                //convert 24-hour to 12-hour time
+//                /*if(hour>12)
+//                    hour_string = String.valueOf(hour-12);
+//                if(minute<10){
+//                    minute_string = "0" + String.valueOf(minute);
+//                }*/
+//
+//                pending_intent = PendingIntent.getBroadcast(add_slot.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+//
+//            }
+//        });
         //initialize the stop Button
         Button stop_alarm = (Button) findViewById(R.id.stop_button);
 
-        stop_alarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alarm_manager.cancel(pending_intent);
-            }
-        });
+//        stop_alarm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                alarm_manager.cancel(pending_intent);
+//            }
+//        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 0:
+                return new TimePickerDialog(this,
+                        mTimeSetListener, pHour, pMinute, false);
+
+        }
+        return null;
     }
 }
